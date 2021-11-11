fea672c4da4c038dfb06e287c5c657ec5ba3b151
erickarodoom <erickarodom@gmail.com>
2021-11-11 10:55:21 -0800

    first commit

---
+++ src/index.js
@@ -0,0 +1,28 @@
+
+
+// -------------------------------------------------
+// const $ = jQuery;
+
+// $(function($){
+
+// });
+
+// Wrap every letter in a span
+var textWrapper = document.querySelector('.ml9 .letters');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: true})
+  .add({
+    targets: '.ml9 .letter',
+    scale: [0, 1],
+    duration: 4000,
+    elasticity: 600,
+    delay: (el, i) => 45 * (i+1)
+  }).add({
+    targets: '.ml9',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 3500
+  });
+
