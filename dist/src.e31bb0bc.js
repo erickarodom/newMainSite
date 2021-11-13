a159b42a440488c05372560fa167b4581d7d6e40
erickarodoom <erickarodom@gmail.com>
2021-11-12 23:29:31 -0800

    mobile phone landing

--- dist/src.e31bb0bc.js
+++ dist/src.e31bb0bc.js
@@ -132,21 +132,12 @@ $(function ($) {
     autoplay: true,
     autoplaySpeed: 2000
   });
+  setTimeout(function () {
+    $('.skillSlider').css("visibility", "visible");
+  }, 3500);
   $('.sidenav').sidenav();
   $('.tap-target').tapTarget();
-}); // (function($){
-//   $(function(){
-//     $('.sidenav').sidenav();
-//     $('.carousel').carousel(
-//       {indicators: true,
-//       numVisible: 3});
-//     $('.tooltipped').tooltip()
-//     $('.tap-target').tapTarget();
-//   }); 
-//   // end of document ready
-// })
-// (jQuery); // end of jQuery name space
-// Wrap every letter in a span
+}); // Wrap every letter in a span
 
 var textWrapper = document.querySelector('.ml9 .letters');
 textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
@@ -160,89 +151,7 @@ anime.timeline({
   delay: function delay(el, i) {
     return 45 * (i + 1);
   }
-}); // Wrap every letter in a span
-
-var textWrapper = document.querySelector('.ml12');
-textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
-anime.timeline({
-  loop: false
-}).add({
-  targets: '.ml12 .letter',
-  translateX: [40, 0],
-  translateZ: 0,
-  opacity: [0, 1],
-  easing: "easeOutExpo",
-  duration: 1600,
-  delay: function delay(el, i) {
-    return 970 + 30 * i;
-  }
-}); // .add({
-//   targets: '.ml12 .letter',
-//   translateX: [0,-30],
-//   opacity: [1,0],
-//   easing: "easeInExpo",
-//   duration: 1100,
-//   delay: (el, i) => 300 + 30 * i
-// });
-// Wrap every letter in a span
-
-var textWrapper = document.querySelector('.ml13');
-textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
-anime.timeline({
-  loop: false
-}).add({
-  targets: '.ml13 .letter',
-  translateY: [100, 0],
-  translateZ: 0,
-  opacity: [0, 1],
-  easing: "easeOutExpo",
-  duration: 1600,
-  delay: function delay(el, i) {
-    return 1270 + 30 * i;
-  }
-}); // .add({
-//   targets: '.ml13 .letter',
-//   translateY: [0,-100],
-//   opacity: [1,0],
-//   easing: "easeInExpo",
-//   duration: 1200,
-//   delay: (el, i) => 500 + 30 * i
-// });
-// // Wrap every letter in a span
-// var textWrapper = document.querySelector('.ml10 .letters');
-// textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
-// anime.timeline({loop: true})
-//   .add({
-//     targets: '.ml10 .letter',
-//     rotateY: [-90, 0],
-//     duration: 1300,
-//     delay: (el, i) => 50 * i
-//   }).add({
-//     targets: '.ml10',
-//     opacity: 0,
-//     duration: 1000,
-//     easing: "easeOutExpo",
-//     delay: 1000
-//   });
-//   // Wrap every letter in a span
-// var textWrapper = document.querySelector('.ml2');
-// textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
-// anime.timeline({loop: true})
-//   .add({
-//     targets: '.ml2 .letter',
-//     scale: [4,1],
-//     opacity: [0,1],
-//     translateZ: 0,
-//     easing: "easeOutExpo",
-//     duration: 1300,
-//     delay: (el, i) => 250*i
-//   }).add({
-//     targets: '.ml2',
-//     opacity: 0,
-//     duration: 1000,
-//     easing: "easeOutExpo",
-//     delay: 1000
-//   });
+});
 },{}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
@@ -271,7 +180,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64853" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59709" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
--- src/index.js
+++ src/index.js
@@ -18,25 +18,14 @@ $(function($){
       
     }
   );
+  setTimeout(function(){
+    $('.skillSlider').css("visibility", "visible");
+  }, 3500);
+
   $('.sidenav').sidenav();
   $('.tap-target').tapTarget();
 });
 
-
-
-// (function($){
-//   $(function(){
-//     $('.sidenav').sidenav();
-//     $('.carousel').carousel(
-//       {indicators: true,
-//       numVisible: 3});
-//     $('.tooltipped').tooltip()
-//     $('.tap-target').tapTarget();
-
-//   }); 
-//   // end of document ready
-// })
-// (jQuery); // end of jQuery name space
 
 // Wrap every letter in a span
 var textWrapper = document.querySelector('.ml9 .letters');
@@ -50,90 +39,3 @@ anime.timeline({loop: false})
     elasticity: 600,
     delay: (el, i) => 45 * (i+1)
   });
-
- 
-
-// Wrap every letter in a span
-var textWrapper = document.querySelector('.ml12');
-textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
-
-anime.timeline({loop: false})
-  .add({
-    targets: '.ml12 .letter',
-    translateX: [40,0],
-    translateZ: 0,
-    opacity: [0,1],
-    easing: "easeOutExpo",
-    duration: 1600,
-    delay: (el, i) => 970 + 30 * i
-  });
-  // .add({
-  //   targets: '.ml12 .letter',
-  //   translateX: [0,-30],
-  //   opacity: [1,0],
-  //   easing: "easeInExpo",
-  //   duration: 1100,
-  //   delay: (el, i) => 300 + 30 * i
-  // });
-
-  // Wrap every letter in a span
-var textWrapper = document.querySelector('.ml13');
-textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
-
-anime.timeline({loop: false})
-  .add({
-    targets: '.ml13 .letter',
-    translateY: [100,0],
-    translateZ: 0,
-    opacity: [0,1],
-    easing: "easeOutExpo",
-    duration: 1600,
-    delay: (el, i) => 1270 + 30 * i
-  });
-  // .add({
-  //   targets: '.ml13 .letter',
-  //   translateY: [0,-100],
-  //   opacity: [1,0],
-  //   easing: "easeInExpo",
-  //   duration: 1200,
-  //   delay: (el, i) => 500 + 30 * i
-  // });
-
-// // Wrap every letter in a span
-// var textWrapper = document.querySelector('.ml10 .letters');
-// textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
-
-// anime.timeline({loop: true})
-//   .add({
-//     targets: '.ml10 .letter',
-//     rotateY: [-90, 0],
-//     duration: 1300,
-//     delay: (el, i) => 50 * i
-//   }).add({
-//     targets: '.ml10',
-//     opacity: 0,
-//     duration: 1000,
-//     easing: "easeOutExpo",
-//     delay: 1000
-//   });
-
-//   // Wrap every letter in a span
-// var textWrapper = document.querySelector('.ml2');
-// textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
-
-// anime.timeline({loop: true})
-//   .add({
-//     targets: '.ml2 .letter',
-//     scale: [4,1],
-//     opacity: [0,1],
-//     translateZ: 0,
-//     easing: "easeOutExpo",
-//     duration: 1300,
-//     delay: (el, i) => 250*i
-//   }).add({
-//     targets: '.ml2',
-//     opacity: 0,
-//     duration: 1000,
-//     easing: "easeOutExpo",
-//     delay: 1000
-//   });
